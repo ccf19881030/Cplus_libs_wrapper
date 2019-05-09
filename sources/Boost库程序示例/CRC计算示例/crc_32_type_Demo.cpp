b70d6074b4800b841dd40c4d3bc1106918ac68df
ccf19881030.github.io <ccf19881030@users.noreply.github.com>
2019-05-09 09:17:46 +0800

    Update crc_32_type_Demo.cpp

--- sources/Boost库程序示例/CRC计算示例/crc_32_type_Demo.cpp
+++ sources/Boost库程序示例/CRC计算示例/crc_32_type_Demo.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 void crc32_Demo1()
 {
-	crc_32_type crc32;						      	      // 一个crc对象
+	crc_32_type crc32;							// 一个crc对象
 
-	cout << hex;								                // 置输出流为hex格式输入
-	cout << crc32.checksum() << endl;			      // 无数据输入时也可以取crc值
+	cout << hex;								// 置输出流为hex格式输入
+	cout << crc32.checksum() << endl;			// 无数据输入时也可以取crc值
 
-	crc32.process_byte('a');					          // 计算一个字节
+	crc32.process_byte('a');					// 计算一个字节
 	cout << crc32.checksum() << endl;
 
 	crc32.process_bytes("1234567890", 10);
 	cout << crc32.checksum() << endl;
 
-	char szChar[] = "1234567890";			        	// 一个字符数组
-	crc32.reset();								              // 复位crc处理机
+	char szChar[] = "1234567890";				// 一个字符数组
+	crc32.reset();								// 复位crc处理机
 
-	crc32.process_block(szChar, szChar + 10);	  // 使用区间的形式输入数据
+	crc32.process_block(szChar, szChar + 10);	// 使用区间的形式输入数据
 	cout << crc32.checksum() << endl;
 }
 
@@ -32,14 +32,14 @@ crc_opotimal还重载了括号操作符operator()，它有两种用法：
 ********************************************************************************/
 void crc32_demo2()
 {
-	crc_32_type crc32;							              // 一个crc对象
+	crc_32_type crc32;							// 一个crc对象
 
 	cout << hex;
-	crc32.process_bytes("1234567890", 10);		    // 处理10个字节
-	cout << crc32() << endl;					            // 使用operator()获得crc值
+	crc32.process_bytes("1234567890", 10);		// 处理10个字节
+	cout << crc32() << endl;					// 使用operator()获得crc值
 
 	std::string str = "1234567890";
-	crc32.reset();								                // 复位crc处理机
+	crc32.reset();								// 复位crc处理机
 	//把crc对象作为函数对象传递给for_each算法处理字符串
 	cout << std::for_each(str.begin(), str.end(), crc32)() << endl;
 	cout << crc32() << endl;

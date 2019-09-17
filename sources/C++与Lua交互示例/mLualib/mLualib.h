333ffa0da1bf102f23afcb7fb113887f72e7a053
ccf19881030.github.io <ccf19881030@users.noreply.github.com>
2019-09-17 15:23:23 +0800

    Update mLualib.h
    修改头文件mLualib.h

--- sources/C++与Lua交互示例/mLualib/mLualib.h
+++ sources/C++与Lua交互示例/mLualib/mLualib.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef _MLUALIB_H_
+#define _MLUALIB_H_
 
 extern "C"
 {
@@ -17,5 +18,7 @@ extern "C"
 	#define DLL_LUA_API extern "C"
 #endif
 
-// 定义导出函数
+// 瀹氫箟瀵煎嚭鍑芥暟
 DLL_LUA_API int luaopen_mLualib(lua_State* L);
+
+#endif 	// _MLUALIB_H_
